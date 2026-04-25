961c7a38f9147e54c9ccfa444275d70b5949c7ff
KrisTwom <retisoverminetwom@gmail.com>
2026-04-25 19:11:00 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,7 +9,9 @@ const STATIC_ASSETS = [
 // Install: cache static assets
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+    caches.open(CACHE_NAME).then(cache =>
+      Promise.allSettled(STATIC_ASSETS.map(url => cache.add(url)))
+    )
   );
   self.skipWaiting();
 });
