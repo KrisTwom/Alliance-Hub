b0e5772d9e2fdfe42fd89ea4812b2946b84ca6ce
KrisTwom <retisoverminetwom@gmail.com>
2026-06-23 22:51:37 -0400

    project ref id

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // ============================================================
 //  CONFIG
 // ============================================================
-const SUPABASE_FUNCTION_URL = 'https://<YOUR_PROJECT_REF>.supabase.co/functions/v1/alliance';
+const SUPABASE_FUNCTION_URL = 'https://jiqnyeuumffjpghptmym.supabase.co/functions/v1/alliance';
 
 // ============================================================
 //  STATE
